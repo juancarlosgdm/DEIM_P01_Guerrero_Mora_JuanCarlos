6d649bdc01957719aacb89e520574e12149bcd47
Juan Carlos GdM <juancarlosgdm@gmail.com>
2024-10-01 13:18:56 +0200

    Animaciones del personaje (idle y walk) + UI para el elemento llave del inventario

--- DEIM_P01_Guerrero_Mora_JuanCarlos/Assets/Scripts/Player/PlayerController.cs
+++ DEIM_P01_Guerrero_Mora_JuanCarlos/Assets/Scripts/Player/PlayerController.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 /// <summary>
 /// Controlador del personaje del jugador
-/// Gestiona el movimiento y el salto del personaje
+/// Gestiona el movimiento, salto y caída del personaje, con sus respectivas animaciones
 /// </summary>
 public class PlayerController : MonoBehaviour
 {
     [Tooltip("Referencia al Rigidbody2D del personaje")]
     [SerializeField] private Rigidbody2D rb;
 
+    [Tooltip("Referencia al AnimatorController del personaje")]
+    [SerializeField] private Animator animator;
+
+    [Tooltip("Referencia al SpriteRenderer del personaje")]
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
     [Tooltip("Velocidad (fuerza) de movimiento del personaje")]
     [SerializeField] private float speed;
 
     [Tooltip("Fuerza de salto del personaje")]
     [SerializeField] private float jumpForce;
+
+    [Tooltip("Fuerza de caída del personaje")]
+    [SerializeField] private float fallForce;
 
     /// <summary>
     /// La función de Start se ejecuta únicamente el primer frame que el objeto esté activo
@@ -36,6 +45,9 @@ public class PlayerController : MonoBehaviour
         {
             // Movimiento del personaje hacia la derecha
             rb.AddForce(Vector2.right * speed);
+
+            // Ajusta la orientación del sprite del personaje hacia la derecha
+            spriteRenderer.flipX = true;
         }
 
         // Comprueba si el jugador ha pulsado la tecla A
@@ -43,6 +55,9 @@ public class PlayerController : MonoBehaviour
         {
             // Movimiento del personaje hacia la izquierda
             rb.AddForce(Vector2.left * speed);
+
+            // Ajusta la orientación del sprite del personaje hacia la izquierda
+            spriteRenderer.flipX = false;
         }
 
         // Comprueba si el jugador ha pulsado la tecla espacio
@@ -50,6 +65,17 @@ public class PlayerController : MonoBehaviour
         {
             // Salto del personaje
             rb.AddForce(Vector2.up * jumpForce);
+        }
+
+        // Gestión de las animaciones de andar del personaje (cualquiera de las dos opciones es válida)
+        animator.SetBool("walking", rb.velocity.x != 0); // Notifica al AnimatorController si el personaje está o no en movimiento en el eje X
+        animator.SetFloat("velocity", rb.velocity.x); // Notifica al AnimatorController la velocidad de movimiento del personaje en el eje X
+
+        // Comprueba si el personaje está cayendo (sentido negativo en eje Y)
+        if (rb.velocity.y < 0)
+        {
+            // Aplica una fuerza extra a la caída
+            rb.AddForce(Vector2.down * fallForce);
         }
     }
 }
--- DEIM_P01_Guerrero_Mora_JuanCarlos/Assets/Scripts/Player/PlayerInventory.cs
+++ DEIM_P01_Guerrero_Mora_JuanCarlos/Assets/Scripts/Player/PlayerInventory.cs
@@ -2,8 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Gestión del inventario del jugador
+/// </summary>
 public class PlayerInventory : MonoBehaviour
 {
+    [Tooltip("Referencia al icono de \"Llave\" en la interfaz")]
+    [SerializeField] private GameObject keyIcon;
+
     [Tooltip("Objeto \"Llave\" del inventario")]
     public bool key;
+
+    /// <summary>
+    /// La función de Update se ejecuta cada uno de los frames que el objeto está activo
+    /// </summary>
+    private void Update()
+    {
+        // Visibilidad del icono de la llave según el jugador la tenga o no (opción 1)
+        keyIcon.SetActive(key);
+
+        // Visibilidad del icono de la llave según el jugador la tenga o no (opción 2)
+        if (key)
+        {
+            keyIcon.SetActive(true);
+        }
+        else
+        {
+            keyIcon.SetActive(false);
+        }
+    }
 }
